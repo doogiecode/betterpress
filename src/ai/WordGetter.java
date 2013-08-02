9c4737745254fe9abfa606822870fa8ce5977442
Richard Booth <rbooth@Richards-MacBook-Pro-2.local>
2013-08-02 14:54:22 -0400

    getwords should be fixed

--- src/ai/WordGetter.java
+++ src/ai/WordGetter.java
@@ -1,5 +1,6 @@
 package ai;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,33 +8,43 @@ public class WordGetter {
 	public static Set<int[][]> getPlays(char[][] letterBoard, Set<String> dict) {
 		HashSet<int[][]> plays = new HashSet<int[][]>();
 		for (String word : dict) {
-			plays.addAll(canMake(letterBoard, word, 0, new int[word.length()][2]));
+			plays.addAll(canMake(letterBoard, word, 0,
+					new int[word.length()][2]));
 		}
 		return plays;
 	}
-	
-	private static Set<int[][]> canMake(char[][] letterBoard, String word, int index, int[][] play) {
+
+	private static Set<int[][]> canMake(char[][] letterBoard, String word,
+			int index, int[][] play) {
+		if (index == word.length()) {
+			return new HashSet<int[][]>();
+		}
+		System.out.println(word);
+		System.out.println(index);
+		System.out.println(Arrays.toString(play));
+
 		char[] wordarray = word.toCharArray();
+		char next = wordarray[index];
 		char[][] copyBoard;
 		int[][] updatedPlay = play;
 		Set<int[][]> plays = new HashSet<int[][]>();
 		int ways = 1;
-		for (char next : wordarray) {
-			for (int i = 0; i < 5; ++i) {
-				for (int j = 0; j < 5; ++j) {
-					if (letterBoard[i][j] == next) {
-						copyBoard = letterBoard;
-						copyBoard[i][j] = ' ';
-						updatedPlay[index][0] = i;
-						updatedPlay[index][0] = j;
-						plays.addAll(canMake(copyBoard, word, index+1, updatedPlay));
-						copyBoard[i][j] = letterBoard[i][j];
-					}
-					
-					
+
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+				if (letterBoard[i][j] == next) {
+					copyBoard = letterBoard;
+					copyBoard[i][j] = ' ';
+					updatedPlay[index][0] = i;
+					updatedPlay[index][0] = j;
+					plays.addAll(canMake(copyBoard, word, index + 1,
+							updatedPlay));
+					copyBoard[i][j] = letterBoard[i][j];
 				}
+
 			}
 		}
+
 		return plays;
 	}
 }
