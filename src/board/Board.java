3a62c74c4800d5f3db2e55824011b1c12d56af94
Richard Booth <rbooth@Richards-MacBook-Pro-2.local>
2013-08-03 11:05:13 -0400

    pushing from a plane in the air a lot high. added reasonable board production and end of game through empty playable word list detection.

--- src/board/Board.java
+++ src/board/Board.java
@@ -19,17 +19,17 @@ import ai.WordGetter;
 
 public class Board {
 
-    private char[][] letterBoard;
+	private char[][] letterBoard;
 	private char[][] colorBoard;
 	private Set<int[][]> playableWords;
 	boolean verbose;
-	
+
 	// used for generating random boards
 	private Random random;
-	
+
 	// all words that are allowed, initialized from dictfile
 	private HashSet<String> dict;
-	
+
 	// words that have been played already
 	private HashSet<String> usedWords;
 	int pass = 0;
@@ -40,128 +40,162 @@ public class Board {
 
 	// Letter representing whose turn it is
 	private char turn;
-	
-	   // Constructor. Random characters, no players constructed yet. Blue goes first.
-    // Dictionary is read in in this constructor, exception currently unhandled.
-    public Board(boolean verbose, boolean easyBoard) {
-        InputStreamReader isr = new InputStreamReader(getClass().getResourceAsStream("/resources/en.txt"));
-        random = new Random();
-        this.verbose = verbose;
-        this.dict = new HashSet<String>(100000);
-        this.usedWords = new HashSet<String>();
-        BufferedReader br;
-        try {
-            br = new BufferedReader(isr);
 
-            String nextline = "";
+	// Constructor. Random characters, no players constructed yet. Blue goes
+	// first.
+	// Dictionary is read in in this constructor, exception currently unhandled.
+	public Board(boolean verbose, boolean easyBoard) {
+		InputStreamReader isr = new InputStreamReader(getClass()
+				.getResourceAsStream("/resources/en.txt"));
+		random = new Random();
+		this.verbose = verbose;
+		this.dict = new HashSet<String>(100000);
+		this.usedWords = new HashSet<String>();
+		BufferedReader br;
+		try {
+			br = new BufferedReader(isr);
 
-            while ((nextline = br.readLine()) != null) {
-                dict.add(nextline);
-            }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+			String nextline = "";
 
-        // Initialize with random
-        this.letterBoard = new char[5][5];
-        this.colorBoard = new char[5][5];
-        
-        for (int i = 0; i < 5; ++i) {
-            for (int j = 0; j < 5; ++j) {
-            	if (easyBoard) {
-            		this.letterBoard[i][j] = ((char) (5*i + j + 'a'));
-            	} else {
-            		this.letterBoard[i][j] = ((char) (random.nextInt(26) + 'a'));
-            	}
-                this.colorBoard[i][j] = '_';
-            }
-        }
-        
-        printLetters();
-        
-        char[][] letterBoardCopy = deepCopy5x5Array(this.letterBoard);
-        this.playableWords = WordGetter.getPlays(letterBoardCopy, this.dict);
-        System.out.println("[BOARD] there are " + this.playableWords.size() + " legal moves");
-        
-        printLetters();
-        
-        // Blue goes first because I said so
-        turn = 'b';
-    }
-    
-    /**
-     * @return the redPlayer
-     */
-    public Player getRedPlayer() {
-        return redPlayer;
-    }
+			while ((nextline = br.readLine()) != null) {
+				dict.add(nextline);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    /**
-     * @param redPlayer the redPlayer to set
-     */
-    public void setRedPlayer(Player redPlayer) {
-        this.redPlayer = redPlayer;
-    }
-    
-    /**
-     * @return the bluePlayer
-     */
-    public Player getBluePlayer() {
-        return bluePlayer;
-    } 
-    
-    /**
-     * @param bluePlayer the bluePlayer to set
-     */
-    public void setBluePlayer(Player bluePlayer) {
-        this.bluePlayer = bluePlayer;
-    }
+		// Initialize with random
+		this.letterBoard = new char[5][5];
+		this.colorBoard = new char[5][5];
 
-    public HashSet<String> getDictionary() {
-        return this.dict;
-    }
-    
-    public void setDictionary(HashSet<String> dict) {
-        this.dict = dict;
-    }
-    
-    /**
-     * @return the letterBoard
-     */
-    public char[][] getLetterBoard() {
-        return letterBoard;
-    }
-    
-    public static char[][] deepCopy5x5Array(char[][] array) {
-    	char[][] copiedArray = new char[5][5];
-    	for (int i = 0; i < 5; ++i) {
-    		for (int j = 0; j < 5; ++j) {
-        		copiedArray[i][j] = array[i][j];
-        	}
-    	}
-    	return copiedArray;
-    }
-  
-    /**
-     * @return the colorBoard
-     */
-    public char[][] getColorBoard() {
-        return colorBoard;
-    }
-	
-    /**
-     * @return the playableWords
-     */
-    public Set<int[][]> getPlayableWords() {
-        return playableWords;
-    }
-
-    public void betterPrint() {
-        System.out.println("[Board] Board State: ");
 		for (int i = 0; i < 5; ++i) {
 			for (int j = 0; j < 5; ++j) {
-				char letter = Character.isUpperCase(colorBoard[i][j]) ? Character.toUpperCase(letterBoard[i][j]) : letterBoard[i][j];
+				if (easyBoard) {
+					this.letterBoard[i][j] = ((char) (5 * i + j + 'a'));
+				} else {
+					this.letterBoard[i][j] = ((char) (random.nextInt(26) + 'a'));
+				}
+				this.colorBoard[i][j] = '_';
+			}
+		}
+
+		printLetters();
+
+		char[][] letterBoardCopy = deepCopy5x5Array(this.letterBoard);
+		this.playableWords = WordGetter.getPlays(letterBoardCopy, this.dict);
+		System.out.println("[BOARD] there are " + this.playableWords.size()
+				+ " legal moves");
+
+		printLetters();
+
+		// Blue goes first because I said so
+		turn = 'b';
+	}
+	
+	private static char[][] reasonableLetterBoard() {
+		Random random = new Random();
+		String consonants = "bcdfghjklmnprstvwxyz";
+		String vowels = "aeiou";
+		List<Character> charSet = new ArrayList<Character>(25);
+		int writeLoc = 0;
+		int numVowels = random.nextInt(4) + 3;
+		for ( ; writeLoc < numVowels; ++writeLoc) {
+			charSet.add(vowels.charAt(random.nextInt(vowels.length())));
+		}
+		if (charSet.contains("u") || charSet.contains("i")) {
+			if (random.nextInt(3) == 0) {
+				++writeLoc;
+				charSet.add('q');
+			}
+		}
+		for ( ; writeLoc < 25; ++writeLoc) {
+			charSet.add(consonants.charAt(random.nextInt(consonants.length())));
+		}
+		char[][] letterArray = new char[5][5];
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+				letterArray[i][j] = charSet.remove(random.nextInt(charSet.size()-1));
+			}
+		}
+		return letterArray;
+	}
+
+	/**
+	 * @return the redPlayer
+	 */
+	public Player getRedPlayer() {
+		return redPlayer;
+	}
+
+	/**
+	 * @param redPlayer
+	 *            the redPlayer to set
+	 */
+	public void setRedPlayer(Player redPlayer) {
+		this.redPlayer = redPlayer;
+	}
+
+	/**
+	 * @return the bluePlayer
+	 */
+	public Player getBluePlayer() {
+		return bluePlayer;
+	}
+
+	/**
+	 * @param bluePlayer
+	 *            the bluePlayer to set
+	 */
+	public void setBluePlayer(Player bluePlayer) {
+		this.bluePlayer = bluePlayer;
+	}
+
+	public HashSet<String> getDictionary() {
+		return this.dict;
+	}
+
+	public void setDictionary(HashSet<String> dict) {
+		this.dict = dict;
+	}
+
+	/**
+	 * @return the letterBoard
+	 */
+	public char[][] getLetterBoard() {
+		return letterBoard;
+	}
+
+	public static char[][] deepCopy5x5Array(char[][] array) {
+		char[][] copiedArray = new char[5][5];
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+				copiedArray[i][j] = array[i][j];
+			}
+		}
+		return copiedArray;
+	}
+
+	/**
+	 * @return the colorBoard
+	 */
+	public char[][] getColorBoard() {
+		return colorBoard;
+	}
+
+	/**
+	 * @return the playableWords
+	 */
+	public Set<int[][]> getPlayableWords() {
+		return playableWords;
+	}
+
+	public void betterPrint() {
+		System.out.println("[Board] Board State: ");
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+				char letter = Character.isUpperCase(colorBoard[i][j]) ? Character
+						.toUpperCase(letterBoard[i][j]) : letterBoard[i][j];
 				if (colorBoard[i][j] == 'b' || colorBoard[i][j] == 'B') {
 					System.out.print("[" + letter + "]");
 				} else if (colorBoard[i][j] == 'r' || colorBoard[i][j] == 'R') {
@@ -173,7 +207,7 @@ public class Board {
 			System.out.println();
 		}
 	}
-	
+
 	public void printLetters() {
 		System.out.println("[Board] Board Letters: ");
 		for (char[] row : this.letterBoard) {
@@ -183,9 +217,9 @@ public class Board {
 			System.out.println();
 		}
 	}
-	
+
 	public void printColors() {
-	    System.out.println("[Board] Board Colors: ");
+		System.out.println("[Board] Board Colors: ");
 		for (char[] row : this.colorBoard) {
 			for (char letter : row) {
 				System.out.print(letter + " ");
@@ -193,21 +227,21 @@ public class Board {
 			System.out.println();
 		}
 	}
-	
+
 	public boolean isValidWord(String word) {
 		return (dict.contains(word) && !usedWords.contains(word));
 	}
 
-	// updates the board to reflect moves, if legal. else, prompts current player.
-	public char playWord(int[][] moves, char playerChar) {		
-		
+	// updates the board to reflect moves, if legal. else, prompts current
+	// player.
+	public char playWord(int[][] moves, char playerChar) {
+
 		String word = whatWordDoesThisPlayMake(moves);
-		
-		
+
 		if (isValidWord(word)) {
 			usedWords.add(word);
 			// Update colorboard to represent move
-			
+
 			this.colorBoard = colorTiles(this.colorBoard, moves, turn);
 
 			switchTurns();
@@ -215,16 +249,18 @@ public class Board {
 				System.out.println("player " + turn + " played " + word);
 				betterPrint();
 			}
-			
+
 			return checkWinner();
 		} else {
-			System.out.println("[BOARD] Player " + turn + " made an illegal move, and so loses.");
+			System.out.println("[BOARD] Player " + turn
+					+ " made an illegal move, and so loses.");
 			switchTurns();
 			return turn;
 		}
 	}
-	
-	public static char[][] colorTiles(char[][] colorBoard, int[][] moves, char playerChar) {
+
+	public static char[][] colorTiles(char[][] colorBoard, int[][] moves,
+			char playerChar) {
 		char[][] copyBoard = deepCopy5x5Array(colorBoard);
 		for (int[] loc : moves) {
 			char target = copyBoard[loc[0]][loc[1]];
@@ -245,17 +281,21 @@ public class Board {
 	}
 
 	// tell whoever's turn it is to provide a move, then plays that move.
-	// char returned is the winner, as determined by checkWinner. Will be ' ' until the game is over.
+	// char returned is the winner, as determined by checkWinner. Will be ' '
+	// until the game is over.
 	private char promptCurrentPlayer() {
 		if (turn == 'r') {
-			return playWord(redPlayer.provideMove(letterBoard, colorBoard, usedWords, turn), turn);
+			return playWord(redPlayer.provideMove(letterBoard, colorBoard,
+					usedWords, turn), turn);
 		} else {
-			return playWord(bluePlayer.provideMove(letterBoard, colorBoard, usedWords, turn), turn);
+			return playWord(bluePlayer.provideMove(letterBoard, colorBoard,
+					usedWords, turn), turn);
 		}
 	}
-	
+
 	// returns the letter of who wins, or ' ' if no one has won yet.
 	private char checkWinner() {
+		
 		int blue = 0;
 		int red = 0;
 		for (char[] row : colorBoard) {
@@ -268,7 +308,8 @@ public class Board {
 				}
 			}
 		}
-		if (blue + red == 25) {
+		
+		if (blue + red == 25 || usedWords.size() == playableWords.size()) {
 			if (red > blue) {
 				return 'r';
 			} else {
@@ -285,7 +326,7 @@ public class Board {
 		for (int i = 0; i < 5; ++i) {
 			for (int j = 0; j < 5; ++j) {
 				// check whether [i][j] should be locked
-				
+
 				// assume locked
 				newColorBoard[i][j] = Character.toUpperCase(colorBoard[i][j]);
 
@@ -306,8 +347,10 @@ public class Board {
 							continue;
 						}
 						// if any don't match, don't lock it
-						if (!charMatch(colorBoard[i][j], colorBoard[i + x][j + y])) {
-							newColorBoard[i][j] = Character.toLowerCase(colorBoard[i][j]);
+						if (!charMatch(colorBoard[i][j], colorBoard[i + x][j
+								+ y])) {
+							newColorBoard[i][j] = Character
+									.toLowerCase(colorBoard[i][j]);
 						}
 					}
 				}
@@ -315,7 +358,7 @@ public class Board {
 		}
 		return newColorBoard;
 	}
-	
+
 	public static boolean charMatch(char char1, char char2) {
 		if (Character.toLowerCase(char1) == Character.toLowerCase(char2)) {
 			return true;
@@ -323,35 +366,35 @@ public class Board {
 			return false;
 		}
 	}
-	
+
 	public String whatWordDoesThisPlayMake(int[][] play) {
 		char[] wordarray = new char[play.length];
 		for (int i = 0; i < play.length; ++i) {
-			wordarray[i] = letterBoard[ play[i][0] ][ play[i][1] ];
+			wordarray[i] = letterBoard[play[i][0]][play[i][1]];
 		}
 		return new String(wordarray);
 	}
-	
-	
 
 	// returns the char of the winner as soon as someone wins.
 	public char playOneGame() {
 		char winner;
-		while ((winner = promptCurrentPlayer()) == ' ');
+		while ((winner = promptCurrentPlayer()) == ' ')
+			;
 		return winner;
 	}
 
 	public static void main(String[] args) {
+		reasonableLetterBoard();
 		Board board = new Board(true, false);
 		board.printColors();
 		board.printLetters();
 		board.betterPrint();
-		
+
 		Bot bluePlayer = new Bot(board);
 		Bot redPlayer = new Bot(board);
 		board.bluePlayer = bluePlayer;
 		board.redPlayer = redPlayer;
-		
+
 		// will currently fail because the Player objects are never initialized
 		System.out.println(board.playOneGame());
 
