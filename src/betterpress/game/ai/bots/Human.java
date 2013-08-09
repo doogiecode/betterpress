e4ec44078b047fb5ebacd39d138f629691a531f1
Richard Booth <rbooth@Richards-MacBook-Pro-2.local>
2013-08-08 22:33:08 -0400

    this should have merged already

---
+++ src/betterpress/game/ai/bots/Human.java
@@ -0,0 +1,15 @@
+package betterpress.game.ai.bots;
+
+import betterpress.game.ai.Player;
+import betterpress.game.letterpress.Board;
+
+public class Human implements Player {
+	private Board board;
+	@Override
+	public int[][] provideMove(char turn) {
+		board.betterPrint();
+		String input;
+		return null;
+	}
+
+}
